def0ccfa49a37a66a27275e6252fcc5625875284
Joe Bruzek <jbruzek@ustwo.com>
2019-05-07 12:04:21 -0400

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by Joe Bruzek on 5/7/19.
+//  Copyright © 2019 Joe Bruzek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
